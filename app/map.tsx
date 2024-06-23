5df71f971106d8ce581a5f7a7bda45b9119a4fec
Cody Marcus <codydavidmarcus@gmail.com>
2024-06-23 17:56:12 -0400

    Remove unused imports

--- app/map.tsx
+++ app/map.tsx
@@ -2,7 +2,6 @@
 
 import { useLayoutEffect } from "react"
 import * as am5 from "@amcharts/amcharts5"
-import * as am5xy from "@amcharts/amcharts5/xy"
 import am5themes_Animated from "@amcharts/amcharts5/themes/Animated"
 import * as am5map from "@amcharts/amcharts5/map"
 import am5geodata_worldLow from "@amcharts/amcharts5-geodata/worldLow"
